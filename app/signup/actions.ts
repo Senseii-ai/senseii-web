f6e43edb5b5075613de606b36cdb516e45f2bc36
Prateek Singh <prateeksingh9741@gmail.com>
2024-09-06 22:12:23 +0530

    modify login signup for using dedicated backend

--- app/signup/actions.ts
+++ app/signup/actions.ts
@@ -1,34 +1,38 @@
 'use server'
 
-import { auth, signIn } from '@/auth'
+import { signIn } from '@/auth'
 import { ResultCode, getStringFromBuffer } from '@/lib/utils'
 import { z } from 'zod'
-import { kv } from '@vercel/kv'
-import { getUser } from '../login/actions'
 import { AuthError } from 'next-auth'
-import { Session } from '@/lib/types'
+import { genSaltSync } from 'bcryptjs'
 
-export async function createUser(email: string, hashedPassword: string) {
-  const existingUser = await getUser(email)
+export async function createUser(
+  email: string,
+  password: string,
+  salt: string
+) {
+  const response = await fetch('http://localhost:9090/api/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      salt: salt
+    })
+  })
 
-  if (existingUser) {
-    return {
-      type: 'error',
-      resultCode: ResultCode.UserAlreadyExists
-    }
-  } else {
-    const user = {
-      id: crypto.randomUUID(),
-      email,
-      password: hashedPassword
-    }
-
-    await kv.hmset(`user:${email}`, user)
-
+  const user = await response.json()
+  if (user.type === 'success') {
     return {
       type: 'success',
       resultCode: ResultCode.UserCreated
     }
+  }
+  return {
+    type: 'failed',
+    resultCode: ResultCode.UnknownError
   }
 }
 
@@ -41,31 +45,23 @@ export async function signup(
   _prevState: Result | undefined,
   formData: FormData
 ): Promise<Result | undefined> {
-  const session = (await auth()) 
   const email = formData.get('email') as string
   const password = formData.get('password') as string
 
-  const response = await fetch('http://localhost:9090/signup', {
-    headers: {
-      Authorization: `Bearer ${}`
-    }
-  })
+  const parsedCredentials = z
+    .object({
+      email: z.string().email(),
+      password: z.string().min(6)
+    })
+    .safeParse({
+      email,
+      password
+    })
 
   if (parsedCredentials.success) {
-    // const salt = crypto.randomUUID()
-    //
-    // const encoder = new TextEncoder()
-    // const saltedPassword = encoder.encode(password + salt)
-    // const hashedPasswordBuffer = await crypto.subtle.digest(
-    //   'SHA-256',
-    //   saltedPassword
-    // )
-    // const hashedPassword = getStringFromBuffer(hashedPasswordBuffer)
-
+    const salt = genSaltSync(10)
     try {
-      // replace this with your Backend call to the DB
-      const result = await createUser(email, password)
-
+      const result = await createUser(email, password, salt)
       if (result.resultCode === ResultCode.UserCreated) {
         await signIn('credentials', {
           email,
