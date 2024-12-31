a892bc12dd8518a19f8f4ae65223e3cb08199326
Prateek Singh <prateeksingh9741@gmail.com>
2024-12-31 08:12:43 +0530

    add chalk package

--- auth.config.ts
+++ auth.config.ts
@@ -1,25 +1,25 @@
-import type { AuthError, DefaultSession, NextAuthConfig } from "next-auth";
-import "next-auth/jwt";
-import Credentials from "next-auth/providers/credentials";
+import type { DefaultSession, NextAuthConfig } from 'next-auth'
+import 'next-auth/jwt'
+import Credentials from 'next-auth/providers/credentials'
 
-import { userLoginDTO } from "@senseii/types";
-import { authAPI } from "./lib/api/auth";
+import { userLoginDTO } from '@senseii/types'
+import { authAPI } from './lib/api/auth'
 
-declare module "next-auth" {
+declare module 'next-auth' {
   interface User {
-    accessToken: string;
-    refreshToken: string;
+    accessToken: string
+    refreshToken: string
   }
 
   interface Session {
-    user: User & DefaultSession["user"];
+    user: User & DefaultSession['user']
   }
 }
 
-declare module "next-auth/jwt" {
+declare module 'next-auth/jwt' {
   interface JWT {
-    accessToken: string;
-    refreshToken: string;
+    accessToken: string
+    refreshToken: string
   }
 }
 
@@ -28,41 +28,47 @@ export default {
   providers: [
     Credentials({
       async authorize(credentials) {
-        const validatedFields = userLoginDTO.safeParse(credentials);
+        const validatedFields = userLoginDTO.safeParse(credentials)
         if (validatedFields.success) {
-
           try {
-            const { email, password } = validatedFields.data;
-            const response = await authAPI.signIn({ email: email, password: password })
+            const { email, password } = validatedFields.data
+            const response = await authAPI.signIn({
+              email: email,
+              password: password
+            })
 
             // OAuth was used to sign in.
             if (!response) {
-              return null;
+              return null
             }
-            return response;
+            return response.data
           } catch (error) {
             return null
           }
         }
-        return null;
-      },
-    }),
+        return null
+      }
+    })
   ],
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token = { ...token, accessToken: user.accessToken, refreshToken: user.refreshToken }
-      } return token
+        token = {
+          ...token,
+          accessToken: user.accessToken,
+          refreshToken: user.refreshToken
+        }
+      }
+      return token
     },
 
     // session transforms the sessoin object.
     async session({ session, token }) {
-
       if (token) {
         session.user.accessToken = token.accessToken
         session.user.refreshToken = token.refreshToken
       }
       return session
     }
-  },
-} satisfies NextAuthConfig;
+  }
+} satisfies NextAuthConfig
