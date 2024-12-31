343ce4a4852a92156316e8a700b2ef4f12d87529
Prateek Singh <prateeksingh9741@gmail.com>
2024-12-31 12:28:10 +0530

    modify api auth routes

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -1,11 +1,18 @@
-import { ResultCode, SignUpFormSchema, UserLoginDTO, userLoginResponseDTO } from "@senseii/types";
-import { axiosInstance, SuccessResponseSchema } from "./http";
-import { z, ZodError } from "zod";
-import { infoLogger } from "../logger/logger";
+import {
+  ResultCode,
+  SignUpFormSchema,
+  UserDTO,
+  userDTOSchema,
+  UserLoginDTO,
+  userLoginResponseDTO
+} from '@senseii/types'
+import { axiosInstance, Result, SuccessResponseSchema } from './http'
+import { z, ZodError } from 'zod'
+import { infoLogger } from '../logger/logger'
 
 export const apiEndpoints = {
   signUp: {
-    response: ResultCode.UserCreated
+    response: userDTOSchema
   },
   signIn: {
     response: userLoginResponseDTO
@@ -15,12 +22,23 @@ export const apiEndpoints = {
 export const authAPI = {
   signUp: async (data: SignUpFormSchema) => {
     try {
-      const response = await axiosInstance.post("/auth/signup", data)
-      const successResponse = SuccessResponseSchema(z.string()).parse(response)
-      return successResponse
+      infoLogger({
+        message: 'singup flow',
+        status: 'INFO',
+        layer: 'ACTION',
+        name: 'authAPI'
+      })
+      const response: Result<UserDTO> = await axiosInstance.post(
+        '/auth/signup',
+        data
+      )
+      if (!response.success) {
+        throw response.error
+      }
+      return response
     } catch (error) {
       if (error instanceof ZodError) {
-        console.error("", error.name)
+        console.error('', error.message)
         return null
       }
       throw error
@@ -28,19 +46,45 @@ export const authAPI = {
   },
   signIn: async (creds: UserLoginDTO) => {
     try {
-      const data = await axiosInstance.post("/auth/login", creds)
-      infoLogger({ message: "signin worked", status: "success", layer: "CONTROLLER", name: "signin" })
-      const successResponse = SuccessResponseSchema(userLoginResponseDTO).safeParse(data)
+      const data = await axiosInstance.post('/auth/login', creds)
+      infoLogger({
+        message: 'signin worked',
+        status: 'success',
+        layer: 'CONTROLLER',
+        name: 'signin'
+      })
+      const successResponse =
+        SuccessResponseSchema(userLoginResponseDTO).safeParse(data)
       if (!successResponse.success) {
-        infoLogger({ message: "signin failed", status: "success", layer: "CONTROLLER", name: "signin" })
+        infoLogger({
+          message: 'signin failed',
+          status: 'success',
+          layer: 'CONTROLLER',
+          name: 'signin'
+        })
       }
       return successResponse.data
     } catch (error) {
       if (error instanceof ZodError) {
-        console.error("validation error", error.name)
+        console.error('validation error', error.name)
         return null
       }
       // error occured on the API layer.
+      throw error
+    }
+  },
+  verifyEmail: async (token: string): Promise<Result<String>> => {
+    try {
+      // TODO: change this to verify-email
+      const response: Result<String> = await axiosInstance.post("/auth/verify-email", { token: token })
+      if (!response.success) {
+        throw response
+      }
+      return response
+    } catch (error) {
+      if (error instanceof ZodError) {
+        console.error("", error.message)
+      }
       throw error
     }
   }
