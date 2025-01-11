da505e9d1b8a0920d9f297d77eb60fda4c7a5871
Prateek Singh <prateeksingh9741@gmail.com>
2025-01-11 19:42:02 +0530

    add senseii types, axios, session cookie handler and HTTP helper

---
+++ app/sessions.ts
@@ -0,0 +1,24 @@
+import { createCookieSessionStorage } from "@remix-run/node"
+import { UserLoginReponseDTO } from "@senseii/types"
+
+const isProduction = process.env.NODE_ENV === "production"
+
+type SessionFlashData = {
+  error: string
+}
+
+// FIX: replace with real secret
+const { getSession, commitSession, destroySession } = createCookieSessionStorage<UserLoginReponseDTO, SessionFlashData>(
+  {
+    cookie: {
+      name: "__session",
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secrets: ['s3cr3t'],
+      ...(isProduction ? { domain: "app.senseii.in", secure: true } : {})
+    }
+  }
+)
+
+export { getSession, commitSession, destroySession }
