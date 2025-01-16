53618c4f361edd5b49b426c1363961fbefed230b
Prateek Singh <prateeksingh9741@gmail.com>
2025-01-16 20:51:56 +0530

    add post wrapper for fetch

--- app/lib/http.ts
+++ app/lib/http.ts
@@ -1,10 +1,13 @@
 import { AppError, Result } from "@senseii/types";
+import { infoLogger } from "./logger";
 
-export const httpGet = async<T>(url: string, token?: string): Promise<Result<T>> => {
+const BE_URL = process.env.BACKEND_URL
+
+export const httpGet = async<T>(url: string, token: string): Promise<Result<T>> => {
   try {
     // FIX: Add a wrapper for missing secret keys.
     // FIX: Add a way to send cookies by default.
-    const fetchUrl = `${process.env.BACKEND_URL}/${url}`
+    const fetchUrl = `${BE_URL}/${url}`
     console.log("Fetching from", fetchUrl)
     const response = await fetch(fetchUrl, {
       method: 'GET',
@@ -31,4 +34,39 @@ export const httpGet = async<T>(url: string, token?: string): Promise<Result<T>>
       error: error as AppError
     }
   }
+}
+
+export const httpPost = async<T>(url: string, token: string, body: Record<string, unknown>): Promise<Result<T>> => {
+  try {
+    const postUrl = `${BE_URL}/${url}`
+    infoLogger({ message: `METHOD: POST URL: ${postUrl}`, status: "alert", layer: "API", name: "HTTP" })
+    const response = await fetch(postUrl, {
+      credentials: "include",
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Bearer ${token}`
+      },
+      method: "POST",
+      body: JSON.stringify(body)
+    })
+    if (!response.ok) {
+      const data = await response.json()
+      console.log("ERROR DATA", data)
+      throw data
+    }
+    const finalResponse = await response.json()
+    return {
+      success: true,
+      data: finalResponse.data
+    }
+  } catch (error) {
+    return {
+      success: false,
+      error: error as AppError
+    }
+  }
+}
+
+export const BE_ROUTES = {
+  createNewGoal: "user/goals/new"
 }
